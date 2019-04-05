0d5a0122d00de700b9fcb343818ca4e14eaa95b3
vontman <vontman@gmail.com>
2019-04-05 22:10:57 +0200

    Update criterion_benchmark.rs

--- benches/criterion_benchmark.rs
+++ benches/criterion_benchmark.rs
@@ -4,12 +4,22 @@ extern crate criterion;
 
 use criterion::Criterion;
 
+
 fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
+    let mut a = 0u64;
+    let mut b = 1u64;
+    let mut c = 0u64;
+
+    if n == 0 {
+        return 0
     }
+
+    for _ in 0..(n+1) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
